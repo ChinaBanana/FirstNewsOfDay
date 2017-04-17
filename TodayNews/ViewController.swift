6683ed064fe2d3584f70a13f9fa8dd36c543ebed
ChinaBanana <cocowu@CocodeMacBook.local>
2017-04-17 13:43:31 +0800

    Initial Commit

---
+++ TodayNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodayNews
+//
+//  Created by Coco Wu on 2017/4/17.
+//  Copyright © 2017年 cyt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
